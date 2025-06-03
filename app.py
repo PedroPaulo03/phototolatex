9a0ebed8373105ca378a758d2a94e2a0385da6ef
PedroPaulo03 <pedropaulog333@gmail.com>
2025-06-03 16:41:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,21 +10,21 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-st.title("✍️ Transforme Notas Manuscritas em markdown")
+st.title("✍️ Transforme Notas Manuscritas em LaTeX")
  
 INSTRUCOES = """
-Você é um assistente de IA que converte texto de anotações manuscritas em formato markdown.
+Você é um assistente de IA que converte texto de anotações manuscritas em formato LaTeX.
 Seu objetivo é representar o texto com precisão, prestando atenção especial às equações matemáticas.
 - Identifique expressões matemáticas e as envolva em `$` para equações inline ou `$$` para equações de exibição.
 Exemplo: `E=mc^2` deve se tornar `$E=mc^2$`
 `integral de a a b f(x) dx` deve se tornar `$$\int_a^b f(x) dx$$`
  - Preserve quebras de parágrafo.
-- Não inclua quaisquer observações introdutórias ou conclusivas, apenas o código markdown puro."""
+- Não inclua quaisquer observações introdutórias ou conclusivas, apenas o código LaTeX puro."""
 
   
 
 
-def generate(imagem_bytes):
+def generate(imagem_bytes, type):
     client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
 
     modelo = "gemini-2.5-flash-preview-05-20"
@@ -33,7 +33,7 @@ def generate(imagem_bytes):
             role="user",
             parts=[
                 types.Part.from_bytes(
-                    mime_type="image/png",
+                    mime_type=type,
                     data=imagem_bytes,
                 ), 
                 types.Part.from_text(text=INSTRUCOES),
@@ -56,42 +56,63 @@ def generate(imagem_bytes):
  
 
 
-imagem_carregada = st.file_uploader(
+imagens_carregadas = st.file_uploader(
     "Selecione uma imagem (.png, .jpeg, .jpg) )",
     type=["png", "jpeg", "jpg"],
-    accept_multiple_files=False # Para este exemplo, apenas um arquivo por vez
+    accept_multiple_files=True # Para este exemplo, apenas um arquivo por vez
 )
 
 col1, col2 = st.columns(2)
 
-if imagem_carregada is not None:
-    with col1:
-        st.subheader("Pré-visualização da Imagem:")
-        # Exibe a imagem carregada. Para PDF, mostrará apenas a primeira página.
-        st.image(imagem_carregada, caption="Sua imagem carregada")#, width =300)
+with col1:
+    st.subheader('Pré-visualização da imagem')
+    imagem_visualizada = st.selectbox(label='Selecione a imagem para pré-visualização',
+                                       format_func=lambda img: img.name if img else "Arquivo desconhecido",
+                                      options = imagens_carregadas, index = 0)
+    if imagem_visualizada is not None:
+        imagem_bytes = imagem_visualizada.getvalue()
+        st.image(imagem_bytes, caption="Imagem selecionada", use_container_width=True)
 
-    st.markdown("---")
-    with col2:  
-        if st.button("Processar Imagem e Gerar LaTeX", key="process_button"):
-            # Garante que o arquivo foi carregado antes de processar
+
+# if imagens_carregadas is not None:
+#     with col1:
+#         st.subheader("Pré-visualização da Imagem:")
+#         # Exibe a imagem carregada. Para PDF, mostrará apenas a primeira página.
+#         st.image(imagens_carregadas, caption="Sua imagem carregada")#, width =300)
+
+with col2:  
+    if st.button("Processar Imagem e Gerar LaTeX", key="process_button"):
+            # Garante que o arquivo foi carregado antes de processa
+        saidas = ''  # Variável para armazenar as saídas LaTeX
+        for i, imagem_carregada in enumerate(imagens_carregadas):
+            
             if imagem_carregada.getvalue():
-    
-                with st.spinner("Convertendo texto para LaTeX...", show_time=True):
+                
+                with st.spinner(f"Convertendo texto da página {i+1} para LaTeX...", show_time=True):
                     file_bytes = imagem_carregada.getvalue()
-
                     try: 
-                        saida = generate(file_bytes)
+                        saida = generate(file_bytes, type=imagem_carregada.type)
+                        saidas += saida + "\n\n"
                     except Exception as e:
                         st.error(f"Erro ao processar a imagem: {e}")
-                        
-                st.markdown("---")
-                st.subheader("Resultado em LaTeX:")             
-                st.markdown(saida)
-                st.code(saida)
-
+                            
             else:
-                st.warning("Nenhum arquivo válido foi carregado para processamento.")
-else:
-    st.info("Por favor, carregue uma imagem ou um arquivo PDF para começar.")
+                    st.warning("Nenhum arquivo válido foi carregado para processamento.")
+
+        # Exibir o resultado em LaTeX
+        st.markdown("---")
+        st.subheader("Resultado em LaTeX:")             
+        st.markdown(saidas)
+        st.code(saidas)
+        # Baixar o resultado em LaTeX
+        st.download_button(
+                        label="Baixar Resultado em LaTeX",
+                        data=saidas,
+                        file_name="resultado_latex.tex",
+                        mime="text/latex"
+                    )       
+    else:
+        st.info("Por favor, carregue uma imagem ou um arquivo PDF para começar.")
+
 
  
